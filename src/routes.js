358ecf6663961a25f36c26fd40ae4345914c23f7
kacung110 <kacung110@DESKTOP-D3K2R47>
2023-05-03 21:52:42 +0700

    tugas

--- src/routes.js
+++ src/routes.js
@@ -14,22 +14,6 @@
                 let path = './public/style.css';
             return h.file(path);
             }
-        },
-        {
-            method:'GET',
-            path:'/esa_unggul.jpg',
-            handler: (request,h) =>{
-                let path = './public/esa_unggul.jpg';
-            return h.file(path);
-            }
-        },
-        {
-            method:'GET',
-            path:'/gambar.jpg',
-            handler: (request,h) =>{
-                let path = './public/gambar.jpg';
-            return h.file(path);
-            }
-        },
+        }
     ];
     module.exports = route;
